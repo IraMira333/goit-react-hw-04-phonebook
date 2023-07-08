94b565474a7f87579e744f411362fb1e9b2374e0
IraMira333 <irinat333@gmail.com>
2023-07-09 01:08:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import css from './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const { contacts } = this.state;
+
+    const isExisting = contacts.find(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (isExisting) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    const contactToAdd = {
+      name: name,
+      number: Number(number),
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contactToAdd],
+    }));
+  };
+
+  removeContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+  onFilterInput = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const filteredContacts = this.getFilterContacts();
+    return (
+      <div className={css.phonebook}>
+        <h2>Phonebook</h2>
+        <ContactForm addContact={this.addContact} />
+
+        <h2>Contacts</h2>
+
+        {filteredContacts > 0 ? (
+          <Filter onFilterInput={this.onFilterInput} />
+        ) : (
+          <p className={css.noContact}>You don't have any contact yet</p>
+        )}
+        <ContactList
+          contacts={filteredContacts}
+          removeContact={this.removeContact}
+        />
+      </div>
+    );
+  }
+}
